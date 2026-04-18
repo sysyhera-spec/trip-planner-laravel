4aac8b9d5f9790e4e68ddf4dc71ac09ffd033c90
Sylvia <sysyhera@gmail.com>
2026-04-18 18:50:56 +0200

    Update DestinationFactory to generate date ranges

--- database/factories/DestinationFactory.php
+++ database/factories/DestinationFactory.php
@@ -1,26 +1,20 @@
 <?php
-
 namespace Database\Factories;
-
 use Illuminate\Database\Eloquent\Factories\Factory;
-use App\Models\Trip;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Destination>
- */
 class DestinationFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
     public function definition(): array
     {
+        $start = $this->faker->dateTimeBetween('now', '+6 months');
+        $end = (clone $start);
+        $end->modify('+' . $this->faker->numberBetween(1, 7) . ' days');
+
         return [
             'name' => $this->faker->city(),
-            'starts_at' => $this->faker->date(),
-            'ends_at' => $this->faker->date(),
+            'starts_at' => $start->format('Y-m-d'),
+            'ends_at' => $end->format('Y-m-d'),
+            'trip_id' => \App\Models\Trip::factory(),
         ];
     }
 }
