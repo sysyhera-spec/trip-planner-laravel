2d25790ff993879f125c79f23e300379cc90a6a8
sysyhera-spec <sysyhera@gmail.com>
2026-04-18 21:48:13 +0200

    Ajout recherche de voyages par titre

--- app/Http/Controllers/TripController.php
+++ app/Http/Controllers/TripController.php
@@ -7,9 +7,15 @@ use App\Models\Trip;
 
 class TripController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $trips = Trip::orderBy('starts_at', 'desc')->get();    
+        $query = Trip::orderBy('starts_at', 'desc');
+
+        if ($request->filled('q')) {
+            $query->where('title', 'like', '%' . $request->q . '%');
+        }
+
+        $trips = $query->get();
         return view('trips.index', ['trips' => $trips]);
     }
 
