0fdd37fe831d9e540d7725cf6dd96b6aa19e529d
Sylvia <sysyhera@gmail.com>
2026-04-18 17:53:00 +0200

    Format Destination model methods for consistency

--- projet-trip-planner/app/Models/Destination.php
+++ projet-trip-planner/app/Models/Destination.php
@@ -8,19 +8,21 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 class Destination extends Model
 {
     use HasFactory;
-    
+
+    protected $fillable = ['trip_id', 'name', 'country', 'arrival_at', 'departure_at'];
+
     public function trip()
-{
-    return $this->belongsTo(Trip::class);
-}
+    {
+        return $this->belongsTo(Trip::class);
+    }
 
-public function activities()
-{
-    return $this->hasMany(Activity::class);
-}
+    public function activities()
+    {
+        return $this->hasMany(Activity::class);
+    }
 
-public function accommodations()
-{
-    return $this->hasMany(Accommodation::class);
-}
+    public function accommodations()
+    {
+        return $this->hasMany(Accommodation::class);
+    }
 }
